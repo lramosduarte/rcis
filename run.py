d9a3805a74893e12c9cb55649a5e84738deb56dc
Leonardo Ramos Duarte <lramosduarte@gmail.com>
2019-03-09 20:41:26 -0300

    Refatoração para estrutura projeto
    Antes o projeto era apenas um script, mas começou a ficar chato de dar
    manutenção. Agora vou transforma-lo em um projeto e estou inciando um
    refactor, adicionando uma estrutura e testes.

--- run.py
+++ run.py
@@ -1,38 +1,89 @@
 import argparse
 import os
+import sys
 import yaml
 
+from datetime import datetime
 from subprocess import Popen
 from time import sleep
 
 
-DADOS = yaml.safe_load(open('teste.yml'))
-PATH_ROOT = DADOS['root']['path']
-DIRETORIOS = {diretorio.name: diretorio.path
-    for diretorio in os.scandir(PATH_ROOT)
-    if diretorio.is_dir()}
+class LogaPIDsEmArquivo:
+
+    def __init__(self, function):
+        self.function = function
+
+    def __call__(self, *args, **kwargs):
+        processos = self.function(*args, **kwargs)
+        diretorio = args[1].rsplit('/')[-1]
+        self.escreve_log_operacao(processos, diretorio)
+
+    def escreve_log_operacao(self, processos, diretorio):
+        msg_log = '\n{datetime} Lista de processos rodando/rodados no {diretorio}:\n{processo}'
+        msg_log = msg_log.format(
+            datetime=datetime.now(), diretorio=diretorio, processo='{processo}')
+        with open('processos_success.log', 'a') as arquivo_pids:
+            for processo in processos:
+                msg_log_processo = '\t{pid} \n{processo}'.format(
+                    pid=processo.pid, processo='{processo}')
+                arquivo_pids.write(msg_log.format(processo=msg_log_processo))
 
 
-diretorio_correto = lambda name, patterns: any(
-    pattern in name for pattern in patterns)
+
+def ler_arquivo(nome_arquivo):
+    return yaml.safe_load(open(nome_arquivo))
 
 
-def rodar_cenario(nome):
-    for step in DADOS['cenario'][nome]:
-        patterns_step = DADOS[step]['pattern']
-        for diretorio, path in DIRETORIOS.items():
-            if (diretorio_correto(diretorio, patterns_step)):
-                process = Popen(
-                    DADOS[step]['command'][0],
-                    cwd=path,
-                    shell=True,
-                )
-                with open('processos.PIDS', 'a') as arquivo_pids:
-                    arquivo_pids.write('{}\n'.format(process.pid))
-        sleep(DADOS[step].get('delay', 1))
+def roda_comandos_no_diretorio(cenario, dados):
+    try:
+        cenario_para_rodar = dados['cenario'][cenario]
+    except KeyError:
+        print('Cenário inválido!')
+        exit(1)
+    diretorios = [diretorio for diretorio in os.scandir(dados['root']['path'])
+                  if diretorio.is_dir()]
+    for comandos in cenario_para_rodar:
+        padroes_nome_diretorio = dados[comandos]['pattern']
+        for diretorio in diretorios:
+            if (diretorio_compativel_com_pattern(diretorio.name, padroes_nome_diretorio)):
+                comandos = dados[diretorio.name]['command']
+                delay = dados[diretorio.name].get('delay')
+                _roda_comandos_em_processo_separado(comandos, diretorio.path, delay)
+
+
+def diretorio_compativel_com_pattern(diretorio, paterns):
+    for p in paterns:
+        if p in diretorio:
+            return True
+    return False
+
+
+@LogaPIDsEmArquivo
+def _roda_comandos_em_processo_separado(comandos, path, delay):
+    def path_arquivo_log(nome_arquivo):
+        return f'{path}/{nome_arquivo}.log'
+
+    processos = []
+    for comando in comandos:
+        with open(path_arquivo_log('stdout'), 'wb') as stdout, open(path_arquivo_log('stderr'), 'wb') as stderr:
+            processos.append(Popen(comandos, cwd=path, shell=True, stdout=stdout, stderr=stderr))
+        sleep(delay or 0)
+    return processos
+
+
+def cria_parser_argumentos_cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('arquivo', help='Nome do arquivo de template', default='template.yml')
+    parser.add_argument('cenario', help='Nome do cenário que será utiilizado!')
+    try:
+        return parser.parse_args()
+    except SystemExit as e:
+        if len(sys.argv) == 1:
+            return parser.parse_args(['template.yml', *sys.argv])
+        raise
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('cenario', help='Nome do cenário que será utiilizado!')
-    args = parser.parse_args()
-    rodar_cenario(args.cenario)
+    args = cria_parser_argumentos_cli()
+    dados = ler_arquivo(args.arquivo)
+    roda_comandos_no_diretorio(args.cenario, dados)
